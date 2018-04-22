00a6fcd6915d2f551a313a9c363b9717e38f0251
Sotiria <s.k.patroni@gmail.com>
2018-04-22 23:01:33 +0100

    change previewClient.jsp, ForwardClient servlet and adress problem in DatabaseManager.java

--- src/org/uniof/manchester/pattern/web/core/ForwardClient.java
+++ src/org/uniof/manchester/pattern/web/core/ForwardClient.java
@@ -1,6 +1,9 @@
 package org.uniof.manchester.pattern.web.core;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.uniof.manchester.pattern.web.Client;
+import org.uniof.manchester.pattern.web.database.DatabaseManager;
 
 //import org.apache.log4j.Logger;
 //import org.uniof.manchester.pattern.web.core.RegisterClient;
@@ -19,8 +26,7 @@ import javax.sql.DataSource;
 @WebServlet("/ForwardClient")
 public class ForwardClient extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	//private static Logger LOG = Logger.getLogger(RegisterClient.class);
+    private static Logger LOG = Logger.getLogger(RegisterClient.class);
  
 	
 	@Resource(name="jdbc/furniture_planner")    
@@ -55,6 +61,7 @@ public class ForwardClient extends HttpServlet {
 		
 		String[] parts = IdName.split(",");
 		String userId = parts[0]; 
+		int clientIdInt = Integer.parseInt(userId);
 		//String firstName = request.setParameter("parts[1]");
 		request.setAttribute("firstName",parts[1]);
 		request.setAttribute("userId",userId);
@@ -62,12 +69,47 @@ public class ForwardClient extends HttpServlet {
 		RequestDispatcher requestDispatcher;
 		//redirect to the order page
 		if (cln == 1) {
-		    requestDispatcher = request.getRequestDispatcher("/previewClient.jsp");
-		}else {
+			
+			Connection conn = null;
+			
+			try {
+					conn  = getConnection();	
+					DatabaseManager dbManager = new DatabaseManager(); 
+			  
+					//ArrayList<Client> clientList = dbManager.getClientsByName(conn, searchName, false);
+					Client client = dbManager.getUserById(conn, clientIdInt);
+					request.setAttribute("client",client);
+					requestDispatcher = request.getRequestDispatcher("/previewClient.jsp");
+			}
+			catch (SQLException e)
+			{
+				LOG.error("Problemas en la generaci�n del excel de calificaciones desde SQL " +  e);
+				throw new RuntimeException("SQL error : " + e.getMessage());
+			} catch (Exception e) {
+				LOG.fatal("Excepci�n al generar el reporte : " + e.getMessage());
+				throw new RuntimeException("Excepci�n al generar el reporte : " + e.getMessage());
+			}
+			finally 
+			{
+				try 
+				{	if(conn!=null){conn.close();}	
+				} catch (SQLException e) 
+				{
+					LOG.error("Problemas al cerrar la conexi�n despues de crear el excel de calificaciones,", e);
+					throw new ServletException(e.getMessage(), e);
+				}
+			}
+		    
+			}
+		else {
 	        requestDispatcher = request.getRequestDispatcher("/order.jsp");
 		}
 		requestDispatcher.forward(request, response);
 		
 	}
 
+		private Connection getConnection() throws SQLException{
+			return dataSource.getConnection();
+		}
+		
 }
